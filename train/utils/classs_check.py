9acde0b0c8901d4e17968b2f465b6c22ee23ed74
Shira Levy <Shira.Levy@osrenterprises.com>
2020-11-08 11:25:50 +0200

    Improved class_check script

--- train/utils/classs_check.py
+++ train/utils/classs_check.py
@@ -1,47 +1,27 @@
-# make sure I have examples of all classes in dataset
-from os import listdir
+# Check how many examples there are of each class
 import argparse
+from os import listdir
+from collections import Counter
 
-# Parse program arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("--file", help="path to file with images paths", default="")
-parser.add_argument("--dir", help="labels directory path", default="")
-parser.add_argument("--classes", help="path to classes.txt file")
+parser.add_argument("dir", help="labels directory path", default="")
+parser.add_argument("classes", help="path to classes.names file")
 args = parser.parse_args()
 
-# classes.txt path
-classes_path = args.classes
+labels_dir = args.dir
+classes = args.classes
 
-with open(classes_path) as f:
-    classes_names = f.readlines()
-num_of_classes = len(classes_names)
-classes_names = [x.strip() for x in classes_names]
+with open(classes) as f:
+    classes_names = f.read().splitlines()
 
-classes = {}
-for i in range(num_of_classes):
-    classes[i] = 0
+labels_files = [x for x in listdir(labels_dir) if x.endswith(".txt")]
 
-if args.file:
-    with open(args.file) as f:
+classes_count = Counter()
+for label_file in labels_files:
+    with open(labels_dir + '/' + label_file) as f:
         lines = f.read().splitlines()
-        imgs_names = [line.split('/')[-1] for line in lines]
-    label_path = '/home/shira/Documents/Projects/OCR/datasets/annotation/train/yolo_new/'
-    for img in imgs_names:
-        with open(label_path + img.split('.')[0] + ".txt") as f:
-            lines = f.readlines()
-            for line in lines:
-                cls = line.split()[0]
-                classes[int(cls)] += 1
+        classes_ids = [x[:2].strip() for x in lines]
+        classes_count.update(Counter(classes_ids))
 
-if args.dir:
-    path_to_files = args.dir
-    for file in listdir(path_to_files):
-        with open(path_to_files + '/' + file) as f:
-            lines = f.readlines()
-            for line in lines:  # TBD: use Counter
-                cls = line.split()[0]
-                classes[int(cls)] += 1
-
-sort_classes = {k: v for k, v in sorted(zip(classes_names, classes.values()), key=lambda item: item[1])}
+sort_classes = {k: v for k, v in sorted(zip(classes_names, classes_count.values()), key=lambda item: item[1])}
 print(sort_classes)
-
